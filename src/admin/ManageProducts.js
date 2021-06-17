4549aff7fad0a3e1c2a801688a7e705b2aa5aca9
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-06-17 23:50:56 +0530

    CRUD complate for products

---
+++ src/admin/ManageProducts.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+// Helpers
+import { isAuthenticated } from "../auth/helper";
+import { deleteProduct, getProducts } from "./helper/adminapicall";
+
+// Base Component
+import Base from "../core/Base";
+
+const ManageProducts = () => {
+  const [products, setProducts] = useState([]);
+  const { user, token } = isAuthenticated();
+
+  const preload = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    preload();
+  }, []);
+
+  const deleteThisProduct = (productId) => {
+    deleteProduct(productId, user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        preload();
+      }
+    });
+  };
+
+  return (
+    <Base
+      title="Welcome admin"
+      description="Manage products here"
+      className="container"
+    >
+      <h2 className="mb-4 text-white">All products:</h2>
+      <Link className="btn btn-info" to={`/admin/dashboard`}>
+        <span className="">Admin Home</span>
+      </Link>
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center text-white my-3">Total 3 products</h2>
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="row text-center mb-2">
+                <div className="col-8">
+                  <h3 className="text-white text-left">{product.name}</h3>
+                </div>
+                <div className="col-2">
+                  <Link
+                    className="btn btn-success"
+                    to={`/admin/product/update/${product._id}`}
+                  >
+                    <span className="">Update</span>
+                  </Link>
+                </div>
+                <div className="col-2">
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => {
+                      deleteThisProduct(product._id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default ManageProducts;
