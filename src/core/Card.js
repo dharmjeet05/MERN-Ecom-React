106fcd9ffa9f09276a82e10b148abcae08944fa7
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-06-22 20:25:29 +0530

    Cart Page is almost ready

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 
 // Helper
 import ImageHelper from "./helper/ImageHelper";
+import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
 
-const Card = ({ product, addToCart = true, removeFromCart = false }) => {
+const Card = ({
+  product,
+  addToCart = true,
+  removeFromCart = false,
+  setReload = (f) => f,
+  reload = undefined,
+}) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
   const cardTitle = product ? product.name : "Default Title";
   const cardDescription = product ? product.description : "Default description";
   const cardPrice = product ? product.price : "DEFAULT";
+
+  const addIntoCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
 
   const showAddToCart = (addToCart) => {
     return (
       addToCart && (
         <button
-          onClick={() => {}}
+          onClick={addIntoCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -25,7 +46,10 @@ const Card = ({ product, addToCart = true, removeFromCart = false }) => {
     return (
       removeCart && (
         <button
-          onClick={() => {}}
+          onClick={() => {
+            removeItemFromCart(product._id);
+            setReload(!reload);
+          }}
           className="btn btn-block btn-outline-danger mt-2 mb-2"
         >
           Remove from cart
@@ -39,11 +63,12 @@ const Card = ({ product, addToCart = true, removeFromCart = false }) => {
       <div className="card text-white bg-dark border border-info">
         <div className="card-header lead">{cardTitle}</div>
         <div className="card-body">
+          {getARedirect(redirect)}
           <ImageHelper product={product} />
           <p className="lead bg-success font-weight-normal text-wrap">
             {cardDescription}
           </p>
-          <p className="btn btn-success rounded btn-sm px-4">$ {cardPrice}</p>
+          <p className="btn btn-success rounded btn-sm px-4">₹ {cardPrice}</p>
           <div className="row">
             <div className="col-12">{showAddToCart(addToCart)}</div>
             <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
---
+++ src/core/Cart.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import "../styles.css";
+
+// Base
+import Base from "./Base";
+
+// Components
+import Card from "./Card";
+
+// Helper
+import { loadCart } from "./helper/cartHelper";
+
+const Cart = () => {
+  const [products, setProducts] = useState([]);
+  const [reload, setReload] = useState(false);
+
+  useEffect(() => {
+    setProducts(loadCart());
+  }, [reload]);
+
+  const loadAllProducts = () => {
+    return (
+      <div>
+        <h2>This section to load products</h2>
+        {products.map((pro, index) => (
+          <Card
+            key={index}
+            product={pro}
+            addToCart={false}
+            removeFromCart={true}
+            setReload={setReload}
+            reload={reload}
+          />
+        ))}
+      </div>
+    );
+  };
+
+  const loadCheckout = () => {
+    return (
+      <div>
+        <h2>This section is for Checkout</h2>
+      </div>
+    );
+  };
+
+  return (
+    <Base title="Cart Page" description="Ready to Checkout">
+      <div className="row text-center">
+        <div className="col-6">{loadAllProducts()}</div>
+        <div className="col-6">{loadCheckout()}</div>
+      </div>
+    </Base>
+  );
+};
+
+export default Cart;
