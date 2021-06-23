5fd48861241c20bb9127280da475e7c2610fd28c
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-06-23 08:20:41 +0530

    Admin Panel typo error done....

--- src/user/AdminDashBoard.js
+++ src/user/AdminDashBoard.js
@@ -18,7 +18,7 @@ const AdminDashBoard = () => {
         <ul className="list-group">
           <li className="list-group-item">
             <Link to="/admin/create/category" className="nav-link text-success">
-              Create Categories
+              Create Category
             </Link>
           </li>
           <li className="list-group-item">
@@ -28,7 +28,7 @@ const AdminDashBoard = () => {
           </li>
           <li className="list-group-item">
             <Link to="/admin/create/product" className="nav-link text-success">
-              Create Products
+              Create Product
             </Link>
           </li>
           <li className="list-group-item">
